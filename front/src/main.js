465200d0286862f024b053b8e910357c2e111db0
Arthur <arthur.dufour@epsi.fr>
2019-04-04 13:31:21 +0200

    added: vuex

--- front/src/main.js
+++ front/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
@@ -9,5 +10,6 @@ Vue.use(VueResource)
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
