a42381fcf3faf37d7b0374cf41ca8a598eea6a27
Arthur <arthur.dufour@epsi.fr>
2019-05-04 23:14:55 +0200

    fixed: linting

--- front/src/components/pages/Recents.vue
+++ front/src/components/pages/Recents.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="text-center mt-5">
-    <div class="error mx-auto " data-text="404">404</div>
-    <p class="lead text-gray-800 mb-5">Page Not Found</p>
-    <p class="text-gray-500 mb-0">Certaines fonctionnalités ne sont pas encore disponibles...</p>
+    <div
+      class="error mx-auto "
+      data-text="404"
+    >
+      404
+    </div>
+    <p class="lead text-gray-800 mb-5">
+      Page Not Found
+    </p>
+    <p class="text-gray-500 mb-0">
+      Certaines fonctionnalités ne sont pas encore disponibles...
+    </p>
     <a href="/#/dashboard?page=dashboard">&larr; Retour au dashboard</a>
   </div>
 </template>
