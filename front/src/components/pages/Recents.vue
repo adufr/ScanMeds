2c6fa61817ef9f37a0f4e41f802a7025f5b33b92
Arthur <arthur.dufour@epsi.fr>
2019-05-04 15:32:56 +0200

    added: mt-5

--- front/src/components/pages/Recents.vue
+++ front/src/components/pages/Recents.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-center">
+  <div class="text-center mt-5">
     <div class="error mx-auto " data-text="404">404</div>
     <p class="lead text-gray-800 mb-5">Page Not Found</p>
     <p class="text-gray-500 mb-0">Certaines fonctionnalités ne sont pas encore disponibles...</p>
